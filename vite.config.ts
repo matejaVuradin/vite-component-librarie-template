340d2181d5a5949f9dc7ea05ef876e21aa6b4382
BeroBurny <bero4net@gmail.com>
2023-07-06 18:41:38 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import {defineConfig} from 'vite'
+import path from 'node:path';
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react'
+
+// TODO: change based on project!
+const name = "MyComponentsLibrary" as const;
+// NOTE: on change update `package.json` to mach!
+const fileName = "my-components-library" as const;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve('./src/lib.ts'),
+      name,
+      formats: ['es', 'umd'],
+      fileName: (format) => `${fileName}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+})
