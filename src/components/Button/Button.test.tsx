49078748e1227afe8810571eaa6d3148a7da63de
Mateja Vuradin <mv53070@fer.hr>
2023-07-11 15:55:31 +0200

    folder for tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Button } from '../src/components/Button';
+import { Button } from '.';
 
 describe('when rendered with  prop', () => {
   it('should render the sending waves button', () => {
